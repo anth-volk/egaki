4c9b59b1760cbefd37dd8030542e28424bf47a3f
Anthony Volk <anth.volk@gmail.com>
2023-11-06 02:02:49 -0600

    fix: Removed unusable code from lexer

--- src/interpretation/lexer.py
+++ src/interpretation/lexer.py
@@ -13,15 +13,9 @@ class Lexer:
     for block in DefaultBlocks:
       tokens = block.tokens
       for token in tokens:
-        lexable_jis[token.ji]: token.role
+        lexable_jis[token["ji"]]: token.role
 
     return lexable_jis
   
   def lex(self, line: str):
-    # Testing to see if jis can be used as keys in Python dict
-    for ji in line:
-      print(ji)
-      if ji in self.lexable_jis.keys():
-        print(f"{ji} found as key")
-      else:
-        print(f"{ji} not found as key")
+    pass
