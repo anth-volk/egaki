5432debe53a612a5c61adaf8946e6de5be0e14c3
Anthony Volk <anth.volk@gmail.com>
2023-11-05 20:12:15 -0600

    feat: Code block class and first version of Numbers block

---
+++ src/blocks/block.py
@@ -0,0 +1,27 @@
+from typing import TypedDict
+
+from src.types.ji_string import JiString
+
+class BlockTokenItem(TypedDict):
+  """
+  Interface for dicts that each functional code
+  block will implement
+  """
+  name: str
+  ji: str
+  role: str
+
+class BlockInterface:
+  """
+  Defines the interface used to 
+  instantiate each functional code block
+  """
+  def __init__(self, indicator_ji: JiString, tokens: list[BlockTokenItem]):
+    self.tokens = tokens
+    self.indicator_ji = indicator_ji
+
+  # indicator ji
+  # tokens
+    # token symbol
+    # token role
+  
